2e3f3530b21e0fa14fbfd7b1f0afbba0d41aca42
ksy <ksy935@mail.ru>
2025-04-10 01:00:11 +0300

    Add Task5,6

---
+++ src/Task5/Program.cs
@@ -0,0 +1,51 @@
+﻿// Пользователь вводит арабское число, на выходе получает римское
+// Допустимый диапазон ввода арабских чисел от 1 до 3999
+
+// Арабские = 1, 2, 3 и т.д
+// Римские = I, II, III и т.д
+
+// 1 - I
+// 5 - V
+// 10 - X
+// 50 - L
+// 100 - C
+// 500 - D
+// 1000 - M
+
+Console.WriteLine("Введите число от 1 до 3999 для преобразования в римскую цифру:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    string roman = ToRoman(number);
+    Console.WriteLine($"Римская запись: {roman}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка: {ex.Message}");
+}
+
+string ToRoman(int number)
+{
+    if (number < 1 || number > 3999)
+    {
+        throw new Exception("Число должно быть от 1 до 3999");
+    }
+
+    int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    string[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+    string result = "";
+
+    for (int i = 0; i < values.Length; i++)
+    {
+        while (number >= values[i])
+        {
+            result += symbols[i];
+            number -= values[i];
+        }
+    }
+
+    return result;
+}
---
+++ src/Task6/SteepArray.cs
@@ -0,0 +1,65 @@
+﻿// Класс должен быть "обвязкой" для массива элементов типа int. И поддерживать следующие методы
+
+// Инициализация массива (на вход подается массив А, элементами которого должен заполниться массив нашего класса)
+// Получение элемента по индексу
+// Получение размера массива (свойство)
+// Добавление элемента в конец массива
+
+// Вставка элемента по индексу, если есть элементы справа, то они должны сдвинуться
+// Удаление элемента по индексу, со сдвигом остальных элементов.
+// Сортировка массива по возрастанию/убыванию
+
+namespace Task6;
+
+/// <summary>
+/// Класс с методами, для работы с массивом
+/// </summary>
+public static class SteepArray
+{
+    private static int[] _array;
+
+    public static int Size {  get { return _array.Length; } }
+
+    /// <summary>
+    /// Метод для инициализации массива
+    /// </summary>
+    /// <param name="nums">Массив пользователя</param>
+    public static void InitializingArray(int[] nums)
+    {
+        nums.CopyTo(_array, 0);
+    }
+
+    /// <summary>
+    /// Метод получения элемента массива
+    /// </summary>
+    /// <param name="index">Индекс элемента</param>
+    public static int GetItemByIndex(int index)
+    {
+        int arrayElement = 0;
+
+        if (index < 0 || index >= _array.Length)
+        {
+            arrayElement = _array[index];
+        }
+        else
+        {
+            Console.WriteLine("Индекс вне границ массива");
+        }
+        return arrayElement;
+    }
+
+    /// <summary>
+    /// Метод добавления элемента в конец
+    /// </summary>
+    /// <param name="num">Добавляемое число</param>
+    public static void AddingToEnd(int num)
+    {
+        int[] nums = [_array.Length + 1];
+
+        _array.CopyTo(nums, 0);
+
+        nums[^1] = num;
+
+        _array = nums;
+    }
+}
